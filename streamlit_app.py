3d7a398de950b067943a8839eec99162e1a633c7
gunalakshmiseran <41740883+gunalakshmiseran@users.noreply.github.com>
2023-06-15 13:07:58 +0530

    Update streamlit_app.py
    removed the unwanted line

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # Just writes data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"Kiwi"")
 
 # take the json version of the response and normalise it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
