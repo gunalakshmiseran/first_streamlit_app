4eae296a0b0fefcc7bd0832cfd01a84c90c19615
gunalakshmiseran <41740883+gunalakshmiseran@users.noreply.github.com>
2023-06-16 19:54:05 +0530

    Update streamlit_app.py
    commit changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ streamlit.dataframe(fruits_to_show)
 
 def fruityvice_data(this_fruit_choice):
 
-    fruityvice_response = requests.get(https://fruityvice.com/api/fruit/+this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
 
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
